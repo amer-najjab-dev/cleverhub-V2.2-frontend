e6443e982e03d8dc912e647ae9b663e6370810f8
Amer Najjab <amer.najjab14@Gmail.com>
2026-03-17 14:52:26 +0000

    fix: activar selector 10/50/100 en historial de ventas

--- src/pages/Home/HomeDashboard.tsx
+++ src/pages/Home/HomeDashboard.tsx
@@ -235,7 +235,10 @@ export const HomeDashboard = () => {
         </div>
 
         {/* Sales History */}
-        <SalesHistory />
+        <SalesHistory 
+          showPagination={true}
+          onViewFullHistory={handleViewFullHistory}
+        />
 
         {/* Charts */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
